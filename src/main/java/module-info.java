adb234c948aaa2e37c7cb8f6300dda98de0cea76
Victor-Algo <victor.algo.contact@gmail.com>
2022-09-14 13:14:53 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.victormeyer.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.victormeyer.javafx to javafx.fxml;
+    exports com.victormeyer.javafx;
+}
